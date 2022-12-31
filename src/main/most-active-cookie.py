b9fc17aa9f40f4e92f7bebe54524372cc208dcb4
LohitReddy359 <kamatham.lohit@gmail.com>
2022-12-31 18:59:06 -0500

    Added Validation Checks to the Command Line Parsing

--- src/main/most-active-cookie.py
+++ src/main/most-active-cookie.py
@@ -1,4 +1,47 @@
 import argparse
+import csv
+import datetime
+
+def displayError(error_message=""):
+    if (error_message != ""):
+        print(error_message)
+    quit()
+
+
+def validateCSVFile(file_name):
+    # Checking if the given file name exists in the directory
+    try:
+        file_obj = open(("../logs/{}".format(file_name)))
+
+    except:
+        print("Given file name doesn't exist in the current directory")
+        return False
+
+    # Checking if the given file name corresponds to a csv file
+    try:
+        csvreader = csv.reader(file_obj)
+        next(csvreader)
+    
+    except:
+        print("Given file name does not correspond to a CSV file")
+        return False
+
+    file_obj.close()
+    return True
+
+
+def validateDate(format, date_string):
+    date_format = "%Y-%m-%d"
+
+    # Checking if given date fits the given format
+    try:
+        datetime.datetime.strptime(date_string, date_format)
+
+    except:
+        return False
+
+    return True
+
 
 def getCommandLineArguments():
     parser = argparse.ArgumentParser(
@@ -20,6 +63,17 @@ def getCommandLineArguments():
         )
 
     args = parser.parse_args()
+
+    # Validating the given file name
+    if (validateCSVFile(args.file_name) == False):
+        # We don't need to add an error message here because the appropriate error
+        # is already displayed while validating
+        displayError()
+
+    # Validating the given date
+    if (validateDate("date", args.date) == False):
+        displayError("Date to search is given in the wrong format. \
+            Correct format: 'YYYY-MM-DD'")
 
     return [args.file_name, args.date]
 
