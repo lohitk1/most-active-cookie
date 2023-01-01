c6829955c1df314a6c027fc27d582fc5d6116de4
LohitReddy359 <kamatham.lohit@gmail.com>
2022-12-31 19:06:17 -0500

    Added Functionality to Extract Cookie Frequency from CSV File

--- src/main/most-active-cookie.py
+++ src/main/most-active-cookie.py
@@ -31,7 +31,11 @@ def validateCSVFile(file_name):
 
 
 def validateDate(format, date_string):
-    date_format = "%Y-%m-%d"
+    if (format == "datetime"):
+        date_format = "%Y-%m-%dT%H:%M:%S%z"
+
+    else:
+        date_format = "%Y-%m-%d"
 
     # Checking if given date fits the given format
     try:
@@ -40,6 +44,19 @@ def validateDate(format, date_string):
     except:
         return False
 
+    return True
+    
+
+def validateCSVHeaders(headers, correct_headers):
+    # Checking whether number of given headers is correct
+    if (len(headers) != len(correct_headers)):
+        return False
+
+    # Checking whether each header is correct
+    for header_index in range(len(headers)):
+        if (headers[header_index] != correct_headers[header_index]):
+            return False
+    
     return True
 
 
@@ -78,12 +95,51 @@ def getCommandLineArguments():
     return [args.file_name, args.date]
 
 
+def extractCookieFrequency(file_name, date_to_search):
+    # Opening the given csv file name to read contents
+    csv_file_obj = open("../logs/{}".format(file_name))
+    csvreader = csv.reader(csv_file_obj)
+
+    # Validating headers
+    headers = next(csvreader)
+    if (validateCSVHeaders(headers, ["cookie", "timestamp"]) == False):
+        displayError(
+            "Headers in CSV file do not fit the problem description. \
+            Make sure you have given the correct csv file."
+        )
+    
+    # Storing the cookies that appear on the date to search and their 
+    # corresponding frequencies on that date in a hashmap
+    cookie_freq_dictionary = dict()
+    row_counter = 1
+    for row in csvreader:
+        if (validateDate("datetime", row[1]) == False):
+            displayError(
+                "Date in row " + row_counter + " of given CSV file \
+                is in the wrong format. \nDate in row " + row_counter + \
+                " of given CSV file of wrong format."
+                    )
+
+        # Modifying the frequency of cookie if the date in current row 
+        # matches the date to search            
+        if (row[1][:10] == date_to_search):
+            cookie_freq_dictionary[row[0]] = 1 + cookie_freq_dictionary.get(row[0], 0)
+        
+        row_counter += 1
+
+    return cookie_freq_dictionary
+
+
 def main():
 
     # Parsing the command line arguments to get the csv file name and date to search for in file
     csv_file_name, date_to_search = getCommandLineArguments()
 
-    print(csv_file_name, date_to_search)
+    # Extracting the cookies and their respective frequencies that occur
+    # on the given date to search, and storing them in a hashmap
+    cookie_freq_dictionary = extractCookieFrequency(csv_file_name, date_to_search)
+
+    print(cookie_freq_dictionary)
 
 
 if __name__ == "__main__":
