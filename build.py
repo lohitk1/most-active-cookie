3c4e71a72d781dacbbeed535ad58a83ae3d5f0d6
LohitReddy359 <kamatham.lohit@gmail.com>
2023-01-01 20:45:17 -0500

    Changed build.py to generate executable file

--- build.py
+++ build.py
@@ -1,16 +1,21 @@
 #   -*- coding: utf-8 -*-
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, after
+
 
 use_plugin("python.core")
 use_plugin("python.unittest")
-use_plugin("python.flake8")
-use_plugin("python.coverage")
-use_plugin("python.distutils")
+use_plugin("python.install_dependencies")
 
 
 name = "most-active-cookie"
 default_task = "publish"
 
+executable_dir = "build/"
+
+executable = [name]
+
+def install_dependencies(project):
+    project.build_depends_on('pyinstaller')
 
 @init
 def set_properties(project):
@@ -18,3 +23,11 @@ def set_properties(project):
     project.set_property("dir_source_unittest_python", "src/unittest")
     project.set_property("dir_source_main_scripts", "src/scripts")
 
+@after("publish")
+def publish(project, logger):
+    import os
+
+    # Creating the executable file
+    from PyInstaller.__main__ import run
+    os.chdir('src/main')
+    run(['--name=%s' % project.name, '--onefile', '--clean', '--distpath=../../', 'most-active-cookie.py'])
